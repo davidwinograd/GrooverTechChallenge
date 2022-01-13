27fae10765d836091ffcfdbf83453dc7188e11b1
David Winograd <davidwinograd@Davids-MacBook-Pro-7.local>
2022-01-12 23:29:50 -0500

    Complete challenge

--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
 import { Track } from "./track";
 
+//create songs
 let track1 = new Track(
     "South of the River",
     new Date("2017-07-05"),
@@ -8,8 +9,50 @@ let track1 = new Track(
     "Geography",
     "R&B");
 
-console.log(JSON.stringify(track1, undefined, 2));
+let track2 = new Track(
+    "In Da Club",
+    new Date("2003-01-07"),
+    193,
+    "50 Cent",
+    "Get Rich or Die Tryin'",
+    "East Coast Hip Hop");
+
+//save initial versions
+track1.save(); 
+track2.save();
+
+//update songs
+track1.name = "North of the River";
+track1.releaseDate = new Date("2017-07-15");
+track1.save(); 
+
+track1.name = "West of the River";
+track1.save();
+
+track1.duration = 279;
+track1.save();
 
 track1.artist = "Thomas Misch";
-track1.genre = "Soul"
-console.log(JSON.stringify(track1, undefined, 2));
+track1.save();
+
+track1.album = "Geography Album";
+track1.save();
+
+track2.name = "In The Club";
+track2.save();
+
+//prints out all versions for user
+track1.printVersions();
+track2.printVersions();
+
+//restore a piece of music to one of its old versions
+track1.load(3);
+track1.print();
+
+track2.load(2);
+track2.print();
+
+
+
+
+
--- track.ts
+++ track.ts
@@ -1,21 +1,97 @@
+import { TrackData } from "./trackData";
+
 class Track {
-    name: string;
-    releaseDate: Date;
-    duration: number;
-    artist: string;
-    album: string;
-    genre: string;
     
+    tdArray: TrackData[];
+    currentTrack: TrackData;
+
     constructor(name: string, releaseDate: Date,
         duration: number, artist: string,
         album: string, genre: string) {
 
-        this.name = name;
-        this.releaseDate = releaseDate;
-        this.duration = duration;
-        this.artist = artist;
-        this.album = album;
-        this.genre = genre;
+        let td = new TrackData(name, releaseDate,
+            duration, artist, album, genre);
+        
+        this.tdArray = [];
+        this.currentTrack = td; 
+    }
+
+
+    public get name(){
+        return this.currentTrack.name;
+    }
+
+    public set name(theName: string){
+        this.currentTrack.name = theName;
+    }
+
+    public get releaseDate(){
+        return this.currentTrack.releaseDate;
+    }
+
+    public set releaseDate(theReleaseDate: Date){
+        this.currentTrack.releaseDate = theReleaseDate;
+    }
+
+    public get duration(){
+        return this.currentTrack.duration;
+    }
+
+    public set duration(theDuration: number){
+        this.currentTrack.duration = theDuration;
+    }
+
+    public get artist(){
+        return this.currentTrack.artist;
+    }
+
+    public set artist(theArtist: string){
+        this.currentTrack.artist = theArtist;
+    }
+
+    public get album(){
+        return this.currentTrack.album;
+    }
+
+    public set album(theAlbum: string){
+        this.currentTrack.album = theAlbum;
+    }
+
+    public get genre(){
+        return this.currentTrack.genre;
+    }
+
+    public set genre(theGenre: string){
+        this.currentTrack.genre = theGenre;
+    }
+
+    public save(){
+        if (this.tdArray.length == 5){
+            this.tdArray.shift();
+        }
+
+        let newTrackData = new TrackData(this.currentTrack.name,
+            this.currentTrack.releaseDate, this.currentTrack.duration, 
+            this.currentTrack.artist, this.currentTrack.album,
+            this.currentTrack.genre);
+
+        this.tdArray.push(newTrackData);
+    }
+
+    public load(version: number){
+        if (version > 5){
+            console.error("Version number must be between 1 and 5, inclusive")
+        }
+        this.currentTrack = this.tdArray[version - 1];
+    }
+
+    public print(){
+        console.log(JSON.stringify(this.currentTrack, undefined, 2));
+    }
+
+    public printVersions(){
+        for (let i = 1; i <= this.tdArray.length; i++)
+            console.log(i, ": ", JSON.stringify(this.tdArray[i-1], undefined, 2));
     }
 }
 
